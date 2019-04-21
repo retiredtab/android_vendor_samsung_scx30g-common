dbcb5bb6744f0a5c0527cbd7ad7d9c5a2412016c
Paulo Sergio Travaglia <pstglia@gmail.com>
2019-04-21 02:02:59 -0300

    vendor/scx30g-common: Remove modemd
    It was causing making T113 kernel unstable:
    
    Oops - BUG: 0 [#1] PREEMPT SMP ARM
    <4>[ 1680.836486]  [0:         modemd:  209] [c0] Modules linked in:
    <4>[ 1680.836517]  [0:         modemd:  209] [c0] CPU: 0 PID: 209 Comm: modemd Tainted: G        W    3.10.17-LineageOS+ #1
    <4>[ 1680.836547]  [0:         modemd:  209] [c0] task: ee5db100 ti: eda14000 task.ti: eda14000
    <4>[ 1680.836578]  [0:         modemd:  209] [c0] PC is at sprd_cproc_native_cp_stop+0x7c/0x14c
    <4>[ 1680.836608]  [0:         modemd:  209] [c0] LR is at console_unlock+0x374/0x3ac
    <4>r: Oops - BUG: 0 [#1] PREEMPT SMP ARM
    <4>[ 1680.836486]  [0:         modemd:  209] [c0] Modules linked in:
    <4>[ 1680.836517]  [0:         modemd:  209] [c0] CPU: 0 PID: 209 Comm: modemd Tainted: G        W    3.10.17-LineageOS+ #1
    <4>[ 1680.836547]  [0:         modemd:  209] [c0] task: ee5db100 ti: eda14000 task.ti: eda14000
    <4>[ 1680.836578]  [0:         modemd:  209] [c0] PC is at sprd_cproc_native_cp_stop+0x7c/0x14c
    <4>[ 1680.836608]  [0:         modemd:  209] [c0] LR is at console_unlock+0x374/0x3ac
    
    Change-Id: I099e2070bb07720ec106092c61c080f4d59b06e1

--- scx30g-common-vendor-blobs.mk
+++ scx30g-common-vendor-blobs.mk
@@ -25,7 +25,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/scx30g-common/proprietary/vendor/firmware/vbc_eq:system/vendor/firmware/vbc_eq \
     vendor/samsung/scx30g-common/proprietary/bin/macloader:system/bin/macloader \
     vendor/samsung/scx30g-common/proprietary/bin/rild:system/bin/rild \
-    vendor/samsung/scx30g-common/proprietary/bin/modemd:system/bin/modemd \
     vendor/samsung/scx30g-common/proprietary/bin/nvitemd:system/bin/nvitemd \
     vendor/samsung/scx30g-common/proprietary/bin/phoneserver:system/bin/phoneserver \
     vendor/samsung/scx30g-common/proprietary/bin/refnotify:system/bin/refnotify \
